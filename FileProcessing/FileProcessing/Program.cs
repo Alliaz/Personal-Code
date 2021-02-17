3df03abed6a0a5bd8e339377c469c2b64e855488
Alliaz <eddiemagnusson95@gmail.com>
2021-02-17 10:27:02 +0100

    Add files via upload

---
+++ FileProcessing/FileProcessing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileProcessing.Business;
+using System.IO;
+
+namespace FileProcessing.View
+{
+    public class Program {
+        public static void Main()
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+            ExceptionHandler exceptionHandler = new ExceptionHandler();
+            try
+            {
+                var splitLine = businessLogic.createSplitString(Console.ReadLine());
+                businessLogic.readFiles(splitLine);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                int exceptionCode = exceptionHandler.NegativeExceptionCodeHandler(e);
+                Console.WriteLine("Error Code: " + "-" + exceptionCode);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
